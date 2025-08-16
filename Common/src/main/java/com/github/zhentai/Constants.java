b21cb49d3662fe1301d463b34b9d4df244ff127d
子峰 徐 <2045228490@qq.com>
2025-08-16 13:34:35 +0800

    Initial commit

---
+++ Common/src/main/java/com/github/zhentai/Constants.java
@@ -0,0 +1,11 @@
+package com.github.zhentai;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class Constants {
+    public static final String MOD_ID = "kaleidoscope_starts";
+    public static final String MOD_NAME = "Kaleidoscope Starts";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
---
+++ NeoForge/src/main/java/com/github/zhentai/KaleidoscopeStartsNeoForge.java
@@ -0,0 +1,15 @@
+package com.github.zhentai;
+
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Constants.MOD_ID)
+public class KaleidoscopeStartsNeoForge {
+
+    public KaleidoscopeStartsNeoForge(IEventBus mEBus, ModContainer modContainer) {
+    }
+}
